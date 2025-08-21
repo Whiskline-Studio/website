725413fd1ce396848bc333a4ccce4aa64b76401e
José Minelli <zecarmineli@gmail.com>
2025-08-21 15:09:16 -0300

    new i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,19 +1,31 @@
+// src/i18n.js
 import { createI18n } from 'vue-i18n';
-
 import ptMessages from './locales/pt.json';
 import enMessages from './locales/en.json';
 
-const savedLanguage = localStorage.getItem('user-language') || 'pt';
+// Fallback inicial
+const defaultLocale = 'pt';
 
+// Cria o i18n
 const i18n = createI18n({
     legacy: false,
-    locale: savedLanguage,
+    locale: defaultLocale,
     fallbackLocale: 'en',
-
     messages: {
         pt: ptMessages,
         en: enMessages,
     },
 });
+
+// Função para carregar o idioma salvo depois do mount
+export function initLocale() {
+    if (typeof localStorage !== 'undefined') {
+        const savedLanguage = localStorage.getItem('user-language');
+        if (savedLanguage === 'pt' || savedLanguage === 'en') {
+            i18n.global.locale.value = savedLanguage;
+        }
+    }
+}
+
 
 export default i18n;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import i18n from './i18n'
+import i18n, { initLocale } from './i18n';
 import '../src/styles/main.css'
 
 const app = createApp(App)
+
 app.use(i18n) 
 app.use(router)
+
+initLocale();
 app.mount('#app')
