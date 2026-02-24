6afeced3403ac81b63f27c7b18b1efb4bf708527
José Minelli <zecarminelli@gmail.com>
2026-02-24 13:06:06 -0300

    Sitemap import fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
